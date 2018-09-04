fb72430120ca649185aaacf549ef880071d1d2cc
Rafael Romancini <rafael.romancini@gmail.com>
2018-09-03 21:26:51 -0300

    Implementing newrelic to ping session on heroku

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+var newrelic = require('./config/newrelic');
 var http = require('http');
 var app = require('./config/express');
+
 if (!process.env.MONGODB_URI){
 	var mongo_uri = 'mongodb://localhost/lects';
 } else{
 	var mongo_uri = process.env.MONGODB_URI;
 }
+
 require('./config/database')(mongo_uri);
 
 http.createServer(app)
