745b3d075f6e82802fc74df1a9b009a615cccaf1
Rafael Romancini <rafael.romancini@gmail.com>
2018-09-09 18:49:33 -0300

    Clean

--- public/js/controllers/homeController.js
+++ public/js/controllers/homeController.js
@@ -9,17 +9,4 @@ angular.module('lects').controller('HomeController', function($scope, resourceLe
 	}, function(error) {
 		console.log(error);
 	});
-
-	// $scope.remover = function(foto) {
-
-	// 	recursoFoto.delete({fotoId: foto._id}, function() {
-	// 		var indiceDaFoto = $scope.fotos.indexOf(foto);
-	// 		$scope.fotos.splice(indiceDaFoto, 1);
-	// 		$scope.mensagem = 'Foto ' + foto.titulo + ' removida com sucesso!';
-	// 	}, function(erro) {
-	// 		console.log(erro);
-	// 		$scope.mensagem = 'Não foi possível apagar a foto ' + foto.titulo;
-	// 	});
-	// };
-
 });
